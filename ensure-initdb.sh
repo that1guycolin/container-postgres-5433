29f4b72d65665595119fca4af2be09fe4fd9c95d
Colin Loeffler <that1guycolin@gmail.com>
2026-08-17 16:45:11 -0500

    refactor(ensure-initdb.sh): most func names still used docker

--- ensure-initdb.sh
+++ ensure-initdb.sh
@@ -8,33 +8,33 @@ if [ "$#" -eq 0 ] || [ "$1" != 'postgres' ]; then
     set -- postgres "$@"
 fi
 
-docker_setup_env
-docker_create_db_directories
+container_setup_env
+container_create_db_directories
 if [ "$(id -u)" = '0' ]; then
     exec gosu postgres "${BASH_SOURCE[@]}" "$@"
 fi
 
 if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
-    docker_verify_minimum_env
-    docker_error_old_databases
+    container_verify_minimum_env
+    container_error_old_databases
 
-    ls /docker-entrypoint-initdb.d/ >/dev/null
+    ls /container-entrypoint-initdb.d/ >/dev/null
 
-    docker_init_database_dir
+    container_init_database_dir
     pg_setup_hba_conf "$@"
 
     export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
-    docker_temp_server_start "$@"
+    container_temp_server_start "$@"
 
-    docker_setup_db
-    docker_process_init_files /docker-entrypoint-initdb.d/*
+    container_setup_db
+    container_process_init_files /container-entrypoint-initdb.d/*
 
-    docker_temp_server_stop
+    container_temp_server_stop
     unset PGPASSWORD
 else
     self="$(basename "$0")"
     case "$self" in
-        docker-ensure-initdb.sh)
+        ensure-initdb.sh)
             echo >&2 "$self: note: database already initialized in '$PGDATA'!"
             exit 0
             ;;
